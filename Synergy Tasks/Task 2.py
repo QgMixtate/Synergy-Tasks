944048145c5e9bd4e2a01f8b149b767a0dc39a84
QgMixtate <cool.roxkiss@yandex.ru>
2024-06-18 10:02:05 +0300

    Revert "Task 1.1"
    This reverts commit debebb293e12a88e5eaa96350ce3a7599001c95a.

--- Synergy Tasks/Task 2.py
+++
@@ -1,9 +0,0 @@
-# coding=windows-1251
-num1 = float(input("Введите длину прямоугольника: "))
-num2 = float(input("Введите ширину прямоугольника: "))
-
-area = num1 * num2
-perimeter = 2 * (num1 + num2)
-
-print("Площадь прямоугольника равна:", area)
-print("Периметр прямоугольника равен:", perimeter)

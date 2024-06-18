8fe127aa6c9929920815a741806bcda64e4071be
QgMixtate <cool.roxkiss@yandex.ru>
2024-06-18 10:04:08 +0300

    Synergy Task

---
+++ Synergy Tasks/Synergy_Tasks1.py
@@ -0,0 +1,7 @@
+# coding=windows-1251
+
+pet_type = input("Вид питомца: ")
+pet_age = input("Возраст питомца: ")
+pet_name = input("Кличка питомца: ")
+
+print(f"Это желторотый - {pet_type} по кличке {pet_name}, Возраст: {pet_age} года.")

44fc2490c3e21243d8fd7761b9ac4bca977f1b83
QgMixtate <cool.roxkiss@yandex.ru>
2024-06-18 10:08:09 +0300

    Reapply "Task 1.1"
    This reverts commit 944048145c5e9bd4e2a01f8b149b767a0dc39a84.

--- Synergy Tasks/Synergy_Tasks.py
+++
@@ -1,7 +0,0 @@
-# coding=windows-1251
-
-pet_type = input("Вид питомца: ")
-pet_age = input("Возраст питомца: ")
-pet_name = input("Кличка питомца: ")
-
-print(f"Это желторотый - {pet_type} по кличке {pet_name}, Возраст: {pet_age} года.")

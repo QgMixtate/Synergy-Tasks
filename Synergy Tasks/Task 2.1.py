2690c337d4149986d5bd0d71ef4c1262f2a2faab
QgMixtate <101570285+QgMixtate@users.noreply.github.com>
2024-06-19 13:41:13 +0300

    Task 2.1.py

--- Synergy Tasks/Task 2.1.py
+++ Synergy Tasks/Task 2.1.py
@@ -7,6 +7,6 @@ hundreds = (number // 100) % 10
 thousands = (number // 1000) % 10
 tenthousands = (number // 10000) % 10
 
-result = (tens ** units) * hundreds * 2 / (tenthousands - thousands)
+result = (tens ** units) * hundreds / (tenthousands - thousands)
 
 print(result)
